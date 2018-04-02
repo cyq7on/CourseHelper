4bbc802d48eaece0a1ce67c5b712fedec4143373
cyq7on <cyq7on@qq.com>
2018-04-02 22:57:04 +0800

    publish task

--- app/src/main/java/com/cyq7on/dap/ui/LookUpTaskActivity.java
+++ app/src/main/java/com/cyq7on/dap/ui/LookUpTaskActivity.java
@@ -11,9 +11,14 @@ import com.cyq7on.dap.R;
 import com.cyq7on.dap.base.ParentWithNaviActivity;
 import com.cyq7on.dap.bean.StudentTaskInfo;
 import com.cyq7on.dap.bean.TeacherTaskInfo;
+import com.cyq7on.dap.event.UserEvent;
+import com.cyq7on.dap.ui.fragment.ReceiverFragment;
 import com.orhanobut.logger.Logger;
 
+import org.greenrobot.eventbus.Subscribe;
+
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.bmob.v3.listener.SaveListener;
 import cn.bmob.v3.listener.UpdateListener;
@@ -35,6 +40,10 @@ public class LookUpTaskActivity extends ParentWithNaviActivity {
     Button btnEdit;
     @Bind(R.id.rlScore)
     RelativeLayout rlScore;
+    @Bind(R.id.tvTarget)
+    EditText tvTarget;
+    @Bind(R.id.rlTarget)
+    RelativeLayout rlTarget;
     private StudentTaskInfo studentTaskInfo;
     private TeacherTaskInfo teacherTaskInfo;
 
@@ -47,10 +56,26 @@ public class LookUpTaskActivity extends ParentWithNaviActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish);
+        ButterKnife.bind(this);
         initNaviView();
+        rlTarget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                FragmentTransaction ft = getFragmentManager().beginTransaction();
+//                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ReceiverFragment receiverFragment = new ReceiverFragment();
+                receiverFragment.show(getFragmentManager(), ReceiverFragment.class.getSimpleName());
+            }
+        });
         Bundle bundle = getBundle();
+        //新建任务
         if (bundle == null) {
             btnEdit.setVisibility(View.GONE);
+            if (user.getRole() == 0) {
+                rlScore.setVisibility(View.GONE);
+            } else {
+                rlTarget.setVisibility(View.GONE);
+            }
         } else {
             btnPublish.setVisibility(View.GONE);
             if (user.getRole() == 0) {
@@ -63,15 +88,18 @@ public class LookUpTaskActivity extends ParentWithNaviActivity {
                     rlScore.setVisibility(View.GONE);
                     tvContent.setText(teacherTaskInfo.content);
                     btnEdit.setVisibility(View.GONE);
+                    rlTarget.setVisibility(View.GONE);
                 } else {
                     studentTaskInfo = (StudentTaskInfo) bundle.getSerializable("info");
                     tvScore.setEnabled(false);
                     tvTitle.setText(studentTaskInfo.title);
                     tvScore.setText(studentTaskInfo.score);
                     tvContent.setText(studentTaskInfo.content);
+                    tvTarget.setText(studentTaskInfo.teacher.getUsername());
                 }
 
             } else {
+                rlTarget.setVisibility(View.GONE);
                 //教师批阅学生作业
                 if (bundle.getBoolean("flag")) {
                     studentTaskInfo = (StudentTaskInfo) bundle.getSerializable("info");
@@ -90,16 +118,32 @@ public class LookUpTaskActivity extends ParentWithNaviActivity {
 
     }
 
+
+    @Subscribe
+    public void onEventMainThread(UserEvent userEvent) {
+        Logger.d(userEvent.user.toString());
+        if(studentTaskInfo == null){
+            studentTaskInfo = new StudentTaskInfo();
+        }
+        studentTaskInfo.teacher = userEvent.user;
+        tvTarget.setText(userEvent.user.getUsername());
+    }
+
     @OnClick(R.id.btn_publish)
     public void onPublishClick(View view) {
-        if (studentTaskInfo != null) {
+        if (user.getRole() == 0) {
+            if(studentTaskInfo == null){
+                studentTaskInfo = new StudentTaskInfo();
+            }
             studentTaskInfo.title = tvTitle.getText().toString();
             studentTaskInfo.content = tvContent.getText().toString();
             studentTaskInfo.score = tvScore.getText().toString();
+            studentTaskInfo.stu = user;
             studentTaskInfo.save(getApplicationContext(), new SaveListener() {
                 @Override
                 public void onSuccess() {
                     toast("发布成功");
+                    finish();
                 }
 
                 @Override
@@ -108,13 +152,18 @@ public class LookUpTaskActivity extends ParentWithNaviActivity {
                     Logger.d(i + s);
                 }
             });
-        } else if (teacherTaskInfo != null) {
+        } else {
+            if(teacherTaskInfo == null){
+                teacherTaskInfo = new TeacherTaskInfo();
+            }
             teacherTaskInfo.title = tvTitle.getText().toString();
             teacherTaskInfo.content = tvContent.getText().toString();
+            teacherTaskInfo.teacher = user;
             teacherTaskInfo.save(getApplicationContext(), new SaveListener() {
                 @Override
                 public void onSuccess() {
                     toast("发布成功");
+                    finish();
                 }
 
                 @Override
@@ -136,6 +185,7 @@ public class LookUpTaskActivity extends ParentWithNaviActivity {
                 @Override
                 public void onSuccess() {
                     toast("更新成功");
+                    finish();
                 }
 
                 @Override
@@ -151,6 +201,7 @@ public class LookUpTaskActivity extends ParentWithNaviActivity {
                 @Override
                 public void onSuccess() {
                     toast("更新成功");
+                    finish();
                 }
 
                 @Override
@@ -161,4 +212,10 @@ public class LookUpTaskActivity extends ParentWithNaviActivity {
             });
         }
     }
+
+    @OnClick(R.id.rlTarget)
+    public void onSelectClick(View view) {
+//        new ReceiverFragment();
+    }
+
 }
